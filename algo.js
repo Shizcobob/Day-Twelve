2ebbdfd9beb986593d4aebd5d43b060fb1802f56
Shizcobob <kyle.korinek@outlook.com>
2023-06-20 12:10:06 -0400

    submission

---
+++ algo.js
@@ -0,0 +1,54 @@
+
+/* 
+    Given a SORTED array of integers, dedupe the array 
+    Because array elements are already in order, all duplicate values will be grouped together.
+
+    Ok to use a new array
+
+    Bonus: (no nested loops, new array ok)
+    Bonus: Do it in-place (no new array)
+
+*/
+
+const nums1 = [1, 1, 1, 1];
+const expected1 = [1];
+
+const nums2 = [1, 1, 2, 2, 3, 3];
+const expected2 = [1, 2, 3];
+
+const nums3 = [1, 1, 2, 3, 3, 4];
+const expected3 = [1, 2, 3, 4];
+
+const nums4 = [1, 1];
+const expected4 = [1];
+
+/**
+ * De-dupes the given sorted array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @returns {Array<number>} The given array deduped.
+ */
+function dedupeSorted(nums) {
+    for(let i = 0; i < nums.length-1; i++){
+        let temp = nums[i+1];
+        if(nums[i] == temp){
+            nums.splice(nums[i], 1)
+        }
+    }return nums
+
+    // Loop through the array, using a for/while loop.
+    // Conditions to look for: Checking if two indexes next to each other are equal. 
+    // If they are not equal -> Push to a new array. 
+
+    // To do this in place, we can use built ins like pop(). 
+
+
+    // Keep in mind, the array is safe to assume as always sorted lowest to highest. 
+
+}
+console.log(dedupeSorted(nums1));
+console.log(dedupeSorted(nums2));
+console.log(dedupeSorted(nums3));
+console.log(dedupeSorted(nums4));
+
